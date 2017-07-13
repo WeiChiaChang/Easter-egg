99bebdba618af0dd2d9f5497dae12ffe155c7555
WesleyChang <ioh04019@gmail.com>
2017-07-13 16:15:36 +0800

    add running-pikachu gif

--- src/running-pikachu.js
+++ src/running-pikachu.js
@@ -2,46 +2,40 @@
 
   "use strict"
 
-  // type 'bravo' on your keyboard
-  let key = [66,82,65,86,79]
+  // type 'pikarun' on your keyboard
+  // let key = [80,73,75,65,82,85,78]
+  let key = [80]
   let ck = 0
   let max = key.length
 
-  let bravo = function () {
+  let pikaRun = function () {
 
     var shock = document.createElement('div')
     var img = new Image()
     img.src = data
-    img.style.width = '374px'
-    img.style.height = '500px'
-    img.style.transition = '1s all'
+    img.style.width = '450px'
+    img.style.height = '350px'
+    img.style.transition = '4s all'
     img.style.position = 'fixed'
-    img.style.left = '-374px'
-    img.style.bottom = 'calc(-50% + 450px)'
+    img.style.left = '-400px'
+    img.style.bottom = 'calc(-50% + 320px)'
     img.style.zIndex = 999999
 
     document.body.appendChild(img)
 
-    window.setTimeout(function(){
-      img.style.left = 'calc(50% - 187px)'
-
-      // $('body').css('background', 'salmon')
-
-    },50)
+    // window.setTimeout(function(){
+    //   img.style.left = 'calc(50% - 200px)'
+    // },50)
 
     window.setTimeout(function(){
-      img.style.left = 'calc(100% + 375px)'
+      img.style.left = 'calc(100% + 500px)'
+    }, 50)
 
-      // $('body').css('background', 'transparent')
-
-    }, 2300)
     window.setTimeout(function(){
       img.parentNode.removeChild(img)
-
-
     }, 4300)
 
-  };
+  }
 
   let record = function(e) {
 
@@ -52,17 +46,17 @@
     }
 
     if ( ck >= max ) {
-      bravo()
+      pikaRun()
       ck = 0
     }
 
-  };
+  }
 
   let init = function (data) {
     document.addEventListener('keyup', record)
-  };
+  }
 
-  let data = './images/bravo.gif'
+  let data = './images/running-pikachu.gif'
 
   init(data)
 
